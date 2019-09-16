c57a12dd53032fe91a22e435c397619e326c48d8
harasou <harasou5@gmail.com>
2019-09-16 11:53:33 +0900

    support go112

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
-func init() {
+func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
